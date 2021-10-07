40b119f5561a81c170014c41496ef7a4be346c6e
Oleksii Trifonov <olexa3wine@yahoo.com>
2021-10-07 16:44:58 +0300

    class HelloWorld was added

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class HelloWorld {
+    private static final Logger logger = LogManager.getLogger(HelloWorld.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello World!!");
+
+    }
+}
